21447c49eb311103736def2ab945d00372fe365c
Relonn <117188565+Relonn@users.noreply.github.com>
2024-12-31 03:39:42 +0300

    update

--- Assets/Editor/StartupSceneLoader.cs
+++ Assets/Editor/StartupSceneLoader.cs
@@ -1,41 +1,37 @@
 using UnityEditor;
 using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
 [InitializeOnLoad]
 public class StartupSceneLoader
 {
+    const string scenePath = "Assets/Scenes/Scene_ErhanVFX.unity";
+
     static StartupSceneLoader()
     {
-        string scenePath = "Assets/WeaponVFX/Scenes/Scene_ErhanVFX.unity";
         EditorSceneManager.sceneOpened += OnSceneOpened;
-        if (EditorSceneManager.GetActiveScene().path != scenePath)
+        if (SceneManager.GetActiveScene().path != scenePath)
         {
             EditorSceneManager.OpenScene(scenePath);
         }
     }
-	private static void OnSceneOpened(Scene scene, OpenSceneMode mode)
+
+    private static void OnSceneOpened(Scene scene, OpenSceneMode mode)
     {
-        Debug.Log($"Scene opened: {scene.name}");
-
-        // Sahneye ait tüm bağımlılıkları bul
+        if (scene.path != scenePath)
+        {
+            EditorSceneManager.OpenScene(scenePath);
+        }
         string[] dependencies = AssetDatabase.GetDependencies(scene.path, true);
-
-        Debug.Log($"Found {dependencies.Length} dependencies for the scene.");
-
         foreach (string dependency in dependencies)
         {
-            // Her bağımlılığı yükle
             Object asset = AssetDatabase.LoadAssetAtPath<Object>(dependency);
             if (asset == null)
             {
                 Debug.LogWarning($"Could not load dependency: {dependency}");
             }
-            else
-            {
-                Debug.Log($"Loaded dependency: {dependency}");
-            }
         }
-
-        Debug.Log("All scene dependencies have been loaded.");
+        SceneManager.SetActiveScene(scene);
     }
 }
